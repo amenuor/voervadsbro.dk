0b02a2ed90b0074d3e8f4b24d34aed295b166791
Angelo <Angelo@Angelos-MacBook-Pro.local>
2015-09-06 21:00:44 +0200

    Added modification to plugin

--- wp-content/themes/voervadsbro.dk/front-page.php
+++ wp-content/themes/voervadsbro.dk/front-page.php
@@ -171,9 +171,17 @@ get_header(); ?>
 		   </div>
 		</div>
 
-<?php
-echo do_shortcode('[fts instagram instagram_id=vovadsbro type=hashtag]');
-?>
+	</section>
+	<svg id="slit" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+		<path id="slitPath2" d="M50 100 C49 80 47 0 40 0 L47 0 Z" />
+		<path id="slitPath3" d="M50 100 C51 80 53 0 60 0 L53 0 Z" />
+		<path id="slitPath1" d="M47 0 L50 100 L53 0 Z" />
+	</svg>
+	<section class="divider ss-style-foldedcorner">
+		<h2>Om #Voervadsbro</h2>
+		<?php
+			echo do_shortcode('[tagregator hashtag="#Voervadsbro"]');
+		?>
 
 	</section>
 <?php get_footer(); ?>
